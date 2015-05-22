4a698f077e5fecf4d1de6c3752e2c507656eac47
Felipe Ramirez <feliperamirezg@gmail.com>
2015-05-22 12:08:31 -0300

    Deleted data

--- src/mobilepsu_api/sql/field.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO mobilepsu_api_field (name) VALUES ('Matemática');
-INSERT INTO mobilepsu_api_field (name) VALUES ('Lenguaje y Comunicación');
-INSERT INTO mobilepsu_api_field (name) VALUES ('Biología');
-INSERT INTO mobilepsu_api_field (name) VALUES ('Física');
-INSERT INTO mobilepsu_api_field (name) VALUES ('Química');
-INSERT INTO mobilepsu_api_field (name) VALUES ('Historia y Ciencias Sociales');
