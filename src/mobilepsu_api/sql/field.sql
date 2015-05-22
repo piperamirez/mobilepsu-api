4be709280a7571bd23bcbb5839a8d141cffaa1b3
Felipe Ramirez <feliperamirezg@gmail.com>
2015-05-21 22:37:39 -0300

    Added sqls to pre-populate the db]

---
+++ src/mobilepsu_api/sql/field.sql
@@ -0,0 +1,6 @@
+INSERT INTO mobilepsu_api_field (name) VALUES ('Matemática');
+INSERT INTO mobilepsu_api_field (name) VALUES ('Lenguaje y Comunicación');
+INSERT INTO mobilepsu_api_field (name) VALUES ('Biología');
+INSERT INTO mobilepsu_api_field (name) VALUES ('Física');
+INSERT INTO mobilepsu_api_field (name) VALUES ('Química');
+INSERT INTO mobilepsu_api_field (name) VALUES ('Historia y Ciencias Sociales');
---
+++ src/mobilepsu_api/sql/topic.sql
@@ -0,0 +1,30 @@
+INSERT INTO mobilepsu_api_topic (field_id, name) VALUES (1, 'Números');
+INSERT INTO mobilepsu_api_topic (field_id, name) VALUES (1, 'Álgebra');
+INSERT INTO mobilepsu_api_topic (field_id, name) VALUES (1, 'Geometría');
+INSERT INTO mobilepsu_api_topic (field_id, name) VALUES (1, 'Datos y Azar');
+
+INSERT INTO mobilepsu_api_topic (field_id, name) VALUES (2, 'Conectores');
+INSERT INTO mobilepsu_api_topic (field_id, name) VALUES (2, 'Plan de redacción');
+INSERT INTO mobilepsu_api_topic (field_id, name) VALUES (2, 'Vocabulario contextual');
+INSERT INTO mobilepsu_api_topic (field_id, name) VALUES (2, 'Comprensión de lectura');
+
+INSERT INTO mobilepsu_api_topic (field_id, name) VALUES (3, 'Organización, estructura y actividad celular');
+INSERT INTO mobilepsu_api_topic (field_id, name) VALUES (3, 'Procesos y funciones vitales');
+INSERT INTO mobilepsu_api_topic (field_id, name) VALUES (3, 'Biología humana y salud');
+INSERT INTO mobilepsu_api_topic (field_id, name) VALUES (3, 'Herencia y evolución');
+INSERT INTO mobilepsu_api_topic (field_id, name) VALUES (3, 'Organismo y ambiente');
+
+INSERT INTO mobilepsu_api_topic (field_id, name) VALUES (4, 'Ondas');
+INSERT INTO mobilepsu_api_topic (field_id, name) VALUES (4, 'Electricidad y magnetismo');
+INSERT INTO mobilepsu_api_topic (field_id, name) VALUES (4, 'Mecánica');
+INSERT INTO mobilepsu_api_topic (field_id, name) VALUES (4, 'Energía');
+INSERT INTO mobilepsu_api_topic (field_id, name) VALUES (4, 'Macroscosmos y microcosmos');
+
+INSERT INTO mobilepsu_api_topic (field_id, name) VALUES (5, 'Estructura atómica');
+INSERT INTO mobilepsu_api_topic (field_id, name) VALUES (5, 'Química orgánica');
+INSERT INTO mobilepsu_api_topic (field_id, name) VALUES (5, 'Reacciones químicas y Estequiometría');
+
+INSERT INTO mobilepsu_api_topic (field_id, name) VALUES (6, 'Espacio Geográfico');
+INSERT INTO mobilepsu_api_topic (field_id, name) VALUES (6, 'El Mundo en perspectiva histórica');
+INSERT INTO mobilepsu_api_topic (field_id, name) VALUES (6, 'Chile y América en perspectiva histórica');
+INSERT INTO mobilepsu_api_topic (field_id, name) VALUES (6, 'Democracia y Desarrollo');
